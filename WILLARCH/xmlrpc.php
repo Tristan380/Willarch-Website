5ff23a1cb07201015cd772a249708c333db243ad
Trista380 <takudzwatristandube@gmail.com>
2024-05-31 15:40:56 +0200

    I want money

--- WILLARCH/xmlrpc.php
+++ WILLARCH/xmlrpc.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://gmcontractors.co.uk</homePageLink>
+		<homePageLink>https://</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://gmcontractors.co.uk/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://gmcontractors.co.uk/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://gmcontractors.co.uk/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://gmcontractors.co.uk/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://gmcontractors.co.uk/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https:///xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https:///xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https:///xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https:///xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https:///wp-json/" />
 			</apis>
 	</service>
 </rsd>
